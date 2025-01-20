b35120556a9e2be062173df72e5288eb9ccb08b6
Temur200511 <tziyodullayev61@gmail.com>
2025-01-20 10:10:42 +0500

    fully

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const addButton = document.getElementById("addTask")
 const taskInput = document.getElementById("taskInput")
 const taskList = document.getElementById("taskList")
 
+loadTasks()
+
 function addTask(){
 const task = taskInput.value.trim();
 if(task){
     createTaskElement(task)
 
     taskInput.value='';
-
+  saveTasks();
     
 }
 else{
@@ -21,9 +23,42 @@ addButton.addEventListener('click', addTask)
 
 function createTaskElement(task){
   
-const listItem = document.createElement("li")
+const listItem = document.createElement('li')
 
 listItem.textContent=task;
 
-taskList.append(listItem)
+const deleteButton = document.createElement('button')
+deleteButton.textContent = 'delete'
+deleteButton.className = 'deleteTask'
+
+listItem.appendChild(deleteButton)
+
+taskList.appendChild(listItem)
+
+deleteButton.addEventListener('click', function(){
+    taskList.removeChild(listItem)
+saveTasks()
 }
+
+)
+
+}
+
+ 
+function saveTasks(){
+    const tasks = []
+    taskList.querySelectorAll('li').forEach(function(item){
+        tasks.push(item.textContent.replace('delete', '').trim())
+    })
+
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function loadTasks(){
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || []
+    tasks.forEach(createTaskElement)
+}
+
+// function deleteTask(){
+//     const
+// }

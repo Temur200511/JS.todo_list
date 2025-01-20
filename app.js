53cf4f26ca6b618504660bf7609d015acc23e865
Temur200511 <tziyodullayev61@gmail.com>
2025-01-20 09:16:20 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const addButton = document.getElementById("addTask")
+const taskInput = document.getElementById("taskInput")
+const taskList = document.getElementById("taskList")
+
+function addTask(){
+const task = taskInput.value.trim();
+if(task){
+    createTaskElement(task)
+
+    taskInput.value='';
+
+    
+}
+else{
+    alert("Enter a task")
+}
+}
+
+addButton.addEventListener('click', addTask)
+
+
+function createTaskElement(task){
+  
+const listItem = document.createElement("li")
+
+listItem.textContent=task;
+
+taskList.append(listItem)
+}
